0fbd2b1687528ffb4a791c04ad54cb13cd6d331e
MegaiSa <95303095+MegaiSa@users.noreply.github.com>
2023-12-14 15:26:06 +0100

    add tf_idf

--- read_file.py
+++ read_file.py
@@ -3,7 +3,7 @@ import os
 
 def file_names(path):
 
-    file_names = [f for f in os.listdir(path) if os.isfile(os.join(path, f))]
+    file_names = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     return file_names
 
 def associate_first_name(name):
@@ -20,21 +20,24 @@ def associate_first_name(name):
 
 def convert(file_list):
     for name in file_list:
-        with open(name) as f:
-            text = f.read(-1)
-        text.lower()
-        cleaned_file = open("cleaned/" + name, "w")
+        with open(name, encoding = "utf-8") as f:
+            text = f.read()
+            for i,l in enumerate(text):
+                if(97 <= ord(l) >= 122):
+                    print(ord(l))
+                    text[i].lower()
+        cleaned_file = open(name, "w", encoding = "utf-8")
         cleaned_file.write(text)
         cleaned_file.close()
 
 def clean(file_name):
-    with open(file_name, "w") as f:
-        text = f.read(-1)
+    with open(file_name, "a+", encoding = "utf-8") as f:
+        text = f.read()
         for l in text:
                 if(letter == "'" or letter == "-" or letter == "\n"):
                     letter = " "
                 if(letter == "." or letter == "," or letter == "!" or letter == "?"):
                     letter = ""
-        text.write(text)
+        f.write(text)
 
 
--- tf-idf.py
+++ tf-idf.py
@@ -1,1 +1,32 @@
+import read_file as rf
+import math as m
+import os
 
+def tf_idf(directory):
+
+    files = rf.file_names(directory)
+    old_dir = os.getcwd()
+    os.chdir(directory)
+    rf.convert(files)
+    os.chdir(old_dir)
+    os.chdir("cleaned")
+    for f in files:
+        rf.clean(f)
+    idf_counter = {}
+    for file in files:
+        with open(file, "r") as f:
+            text = f.read()
+        text.split(" ")
+        for word in text:
+            if word in idf_counter.keys():
+                idf_counter[word] += 1
+            else:
+                idf_counter[word] = 1
+    for w in idf_counter.keys():
+        idf_counter[w] = 1/m.log(idf_counter[w])
+    return idf_counter
+
+if __name__ == "__main__":
+    print(tf_idf("speeches"))
+
+    
